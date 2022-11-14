2902c1f3e30bf187be4665ee69bb3bf5d063f8c3
Thomas Williams <cree83@msn.com>
2022-11-13 21:16:00 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
+import Wishlist from "./wishlist";  
+
 console.log("Hello World");
 
-// TODO
+let wishlist = new Wishlist();
+
+//Select the form
+let form = document.querySelector("#submitForm");
+//Select the input for car make
+let inputMake = document.querySelector("#makeInput");
+//Select the input for car model
+let inputModel = document.querySelector("#modelInput");
+//Select the input for car year
+let inputYear = document.querySelector("#yearInput");
+//Select the paragraph element for car make
+let carMake = document.querySelector("#car-make");
+//Select the paragraph element for car model
+let carModel = document.querySelector("#car-model");
+//Select the paragraph element for car year
+let carYear = document.querySelector("#car-year");
+//Select the remove button
+let removeBtn = document.querySelector("#removeBtn");
+//Select the wishlist unordered list element
+let ulWishList = document.querySelector("#wishListContainer > ul");
+//Call the WishList constructor to create an instance from the WishList class
+
+form.addEventListener("submit", addCar);
+removeBtn.addEventListener("click", removeCar);
+
+function showCarDetails(car){
+    carMake.textContent = car.make;
+    carModel.textContent = car.model;
+    carYear.textContent = car.year;
+
+    removeBtn.disabled = false;
+    removeBtn.setAttribute("data-carId", car.id);
+}
+
+function updateDom() {
+    ulWishList.innerHTML = "";
+
+    wishlist.list.forEach((car) => {
+        let li = document.createElement("li");
+        li.textContent = car.model;
+        li.addEventListener("click", () => showCarDetails(car));
+        ulWishList.appendChild(li);
+    });
+}
+
+function addCar(event) {
+    event.preventDefault();
+
+    let make = inputMake.value;
+    let model = inputModel.value;
+    let year = inputYear.value;
+
+    wishlist.add(make, model, year);
+    updateDom();
+    inputMake.value = "";
+    inputModel.value = "";
+    inputYear.value = "";
+}
+
+function removeCar() {
+    let carId = Number(removeBtn.getAttribute("data-carId"));
+    wishlist.remove(carId);
+
+    updateDom();
+    carMake.textContent = "";
+    carModel.textContent = "";
+    carYear.textContent = "";
+
+    removeBtn.disabled = true;
+}
+
---
+++ src/wishlist.js
@@ -0,0 +1,16 @@
+import Car from "./car";
+
+export default class Wishlist {
+    list = [];
+    nextId = 0;
+    
+    add(make, model, year){
+        let car = new Car(this.nextId++, make, model, year);
+        this.list.push(car);
+    }
+    
+    remove(carId){
+        let index = this.list.findIndex((car) => car.Id == carId);
+        this.list.splice(index, 1);
+    }
+}
